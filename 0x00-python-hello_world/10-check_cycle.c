7b8094ca5e80d71d418634e8bfbdcaf5c8f9a039
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-07 18:19:52 +0100

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+/**
+ * check_cycle - To check node
+ * @list: nodes
+ * Return: 1 or 0
+ */
+
+int check_cycle(listint_t *list)
+{
+	listint_t *slow_node = list;
+	listint_t *fast_node = list;
+
+	if (!list)
+		return (0);
+
+	for (; slow_node && fast_node && fast_node->next;
+			slow_node = slow_node->next, fast_node = fast_node->next->next)
+	{
+		if (slow_node == fast_node)
+			return (1);
+	}
+	return (0);
+}
