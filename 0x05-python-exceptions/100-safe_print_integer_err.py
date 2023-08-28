2885c32adbfc610c6f13f4f38df6333055596864
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-28 17:23:55 +0100

    safe print s

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+    except Exception as te:
+        import sys
+        print("{}".format(te), file=sys.stderr)
+        return False
+    else:
+        return True
