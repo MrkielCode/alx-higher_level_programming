4638697b332d6e8ca2604180966e354c3f38617a
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-28 21:57:42 +0100

    magic cal

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
     result = 0
 
@@ -8,8 +7,8 @@ def magic_calculation(a, b):
             if i > a:
                 raise Exception('Too far')
 
-            result += a ** b / i
+            result = result + (a ** b / i)
         except:
-            result = b + a
-            break
-    return result
+            result = result + (a + b)
+
+        return result
