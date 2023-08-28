ff270bd7397f93c1515e41621796169724c09ffa
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-28 17:24:38 +0100

    safe print s

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -3,7 +3,7 @@
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as te:
+    except (ValueError, TypeError) as te:
         import sys
         print("{}".format(te), file=sys.stderr)
         return False
