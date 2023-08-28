f80424428104d3556a19ed2655fca436129b977b
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-28 15:57:56 +0100

    safe print 3

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     try:
         sum = a / b
     except ZeroDivisionError:
-        sum = "none"
+        sum = None
     finally:
         print("Inside result: {}".format(sum))
     return sum
