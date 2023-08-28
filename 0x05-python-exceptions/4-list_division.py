c9eb613a9bfe6690930a82ca855584bfe081c9c8
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-28 16:20:38 +0100

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
 
@@ -17,4 +18,4 @@ def list_division(my_list_1, my_list_2, list_length):
         finally:
             new_list.append(sumList)
 
-    return new_list
+    return (new_list)
