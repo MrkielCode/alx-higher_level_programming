b02da93cb903ce2b28506628e433efb321dd8907
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-28 14:33:00 +0100

    safe print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]), end="")
             counter += 1
-        print()
         return counter
     except IndexError:
         return counter
+    print()
