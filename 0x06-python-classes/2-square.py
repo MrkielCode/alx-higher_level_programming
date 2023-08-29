4d9e1f63e5445393a0ed79c11c050f2296144795
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-29 16:05:03 +0100

    square 2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/urs/bin/python3
+
+""" This script that perform an exception on sqaure Method """
+
+
+class Square:
+
+    """
+    The class square contains a initialization called size and
+    run exception on it
+    """
+
+    def __init__(self, size=0):
+
+        """
+        Args:
+            size: size of a sqaure
+
+            """
+
+        if type(size) is int:
+
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size
+        else:
+            raise TypeError("size must be an integer")
