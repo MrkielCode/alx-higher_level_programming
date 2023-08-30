67942af0bec093a30de82aea4e4a9371abb67325
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-30 01:05:34 +0100

    6 sqaure

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -35,10 +35,10 @@ class Square:
 
         """ setter of size of sqaure """
 
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if (value < 0):
-            raise Value('size must be an integer')
+            raise ValueError("size must be >= 0")
 
         self.__size = value
 
