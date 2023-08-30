d07aefd9a1b7852e5ddffcfa33f5e6b4138fb4d4
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-30 01:53:18 +0100

    magic class

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Defines a magic class """
+import math
+
+class MagicClass:
+""" class represent a magic class """
+
+    def _init_(self, radius=0):
+        """ initilization of radius """
+        self.__radius = 0
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
+        self.__radius = radius
+
+    def area(self):
+        """ define area of circle """
+        return (self.__radius ** 2) * math.pi
+
+    def circumference(self):
+        """ define area of circle """
+
+        return 2 * math.pi * self.__radius
