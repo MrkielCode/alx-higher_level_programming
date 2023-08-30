1ff1122be49fc0f42bf51e25701cde0bd7e8a89f
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-30 16:13:53 +0100

    list division

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 def list_division(my_list_1, my_list_2, list_length):
+
     new_list = []
 
     for j in range(list_length):
@@ -9,16 +10,20 @@ def list_division(my_list_1, my_list_2, list_length):
 
         except(ValueError, TypeError):
             print("wrong type")
+
             sumList = 0
 
         except ZeroDivisionError:
             print("division by 0")
+
             sumList = 0
 
         except IndexError:
             print("out of range")
+
             sumList = 0
 
         finally:
             new_list.append(sumList)
+
     return (new_list)
