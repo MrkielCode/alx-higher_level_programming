6122d046d51b171be13afed7d4bc462d905ff6f5
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-08-30 02:03:43 +0100

    magic class

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,7 +5,7 @@ import math
 
 class MagicClass:
     """ class represent a magic class """
-    def _init_(self, radius=0):
+    def __init__(self, radius=0):
         """ initilization of radius """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
