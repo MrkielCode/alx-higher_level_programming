ba1bc756d93c9cd3f8a2ec5697375df3e751562a
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-06 00:50:11 +0100

    102-python.c

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,34 @@
+#include "Python.h"
+
+/**
+ * print_python_string - To print infor about python strings
+ * @p: PyObject string object.
+ * Return: nothing
+ */
+void print_python_string(PyObject *p)
+{
+	long int count;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	count = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+	{
+		printf("  type: compact ascii\n");
+	}
+	else
+	{
+		printf("  type: compact unicode object\n");
+	}
+
+	printf("  length: %ld\n", count);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &count));
+}
