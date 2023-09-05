78df57232fb5eab6e14adce36d9ce5984d760afd
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-05 08:19:15 +0100

    push

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+defining a class
+"""
+
+
+class LockedClass:
+
+    """ 
+    class to check instance of Lockedclass
+
+    """
+
+    __slots__ = ('first_name', )
+
+    def __init__(self):
+        self.first_name = None
+
+    def __setattr__(self, name, value):
+        if name != 'first_name':
+            raise AttributeError("'LockedClass' object has no attribute "
+            "'{}'".format(name))
+        super().__setattr__(name, value)
