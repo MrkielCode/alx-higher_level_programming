c7d207d969dc4156a2d96432ede39bc67eb4fba2
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-11 15:34:26 +0100

    4 inherits

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This function checked the instance of
+a class is a direct inherit
+"""
+
 
 def inherits_from(obj, a_class):
+    """
+    return true it inherited directly
+    """
     return issubclass(type(obj), a_class) and type(obj) != a_class
