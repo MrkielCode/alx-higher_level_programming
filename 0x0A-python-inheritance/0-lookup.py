660bad86b57d1d9b35c69f2a90fe4cfaa2e4a374
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-11 14:52:13 +0100

    look up

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -3,10 +3,11 @@
 This function return a list of attributes
 associated with a class
 """
+
+
 def lookup(obj):
     """
     args:
         obj: class object to be checked
     """
     return dir(obj)
-
