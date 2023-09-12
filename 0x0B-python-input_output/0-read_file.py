026c369824f9c5db4aad36b5106964bf9bbbecc2
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-12 15:18:32 +0100

    read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+This function take a parameter and
+return the file read
+"""
+
+
+def read_file(filename=""):
+    """
+    args:
+    filename: file to be read
+
+    """
+
+    with open(filename, encoding="utf-8") as myfile:
+        print(myfile.read())
