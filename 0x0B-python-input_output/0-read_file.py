777659e3f6d7e7e0fcf27f2113b9d1829d39d939
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-12 15:21:11 +0100

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -12,5 +12,5 @@ def read_file(filename=""):
 
     """
 
-    with open(filename, encoding="utf-8") as myfile:
+    with open(filename, mode="r", encoding="utf-8") as myfile:
         print(myfile.read())
