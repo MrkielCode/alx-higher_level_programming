87513a59a9df91c43dc39b235f7d4ba45801a189
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-19 11:57:06 +0100

    sqaure

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,7 +17,10 @@ class Square(Rectangle):
 
     """
     def __init__(self, size, x=0, y=0, id=None):
-        """ initialization of attributes """
+        """
+        initialization of attributes
+
+        """
 
         super().__init__(size, size, x, y, id)
 
