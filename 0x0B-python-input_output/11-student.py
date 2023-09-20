fd042aca56a1e7c37c0a26067a83327fa6e756ab
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-09-20 14:48:41 +0100

    student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+This has list of public attributes that return as
+json
+"""
+
+
+class Student:
+    """
+    method:
+        __int__: intialization of attribute
+    """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is None:
+            return self.__dict__
+        else:
+            attr_dict = {}
+            for attr in attrs:
+                if hasattr(self, attr):
+                    attr_dict[attr] = getattr(self, attr)
+            return attr_dict
+
+    def to_json(self, attrs=None):
+        try:
+            for key, value in json.items():
+                setattr(self, key, value)
+        except FileNotFoundError:
+            pass
