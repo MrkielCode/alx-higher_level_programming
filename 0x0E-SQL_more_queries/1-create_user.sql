73afadce52c16cbdac1c5e56dfcc0d18713a3e14
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-10-18 22:50:13 +0100

    create

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,6 +1,5 @@
 -- creatting of user_0d_1 and granting
 -- all privelages
-
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 FLUSH PRIVILEGES;
