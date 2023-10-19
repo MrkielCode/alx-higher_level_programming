9cc504c7433d313330a9add533e9989866c3933e
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-10-19 15:46:08 +0100

    show genre

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,9 +1,7 @@
 -- to display all genere and its link
 
-SELECT g.name
-FROM tv_shows s
-JOIN tv_show_genres sg
-ON s.id = sg.show_id
-JOIN tv_genres g
-ON g.id = sg.genre_id
-Where s.title = 'Dexter'
+SELECT g.name FROM tv_shows s
+	JOIN tv_show_genres sg ON s.id = sg.show_id
+	JOIN tv_genres g ON g.id = sg.genre_id
+	Where s.title = 'Dexter'
+	ORDER BY g.name ASC
