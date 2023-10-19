5cf6e6ff5b91d50a289761577bf7b7623a7854b2
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-10-19 15:13:28 +0100

    display genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,9 +1,9 @@
 -- to display all genere and its link
 
 SELECT g.name AS genre,
-COUNT(show_id) AS number_of_row
+COUNT(show_id) AS number_of_shows
 FROM tv_genres g
 JOIN tv_show_genres sg
 ON g.id = sg.genre_id
 GROUP BY genre
-ORDER BY number_of_row DESC;
+ORDER BY number_of_shows DESC;
