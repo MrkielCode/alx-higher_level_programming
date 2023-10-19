5f842c3436273002b5c728947043e8ee744bb376
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-10-19 16:28:16 +0100

    list comedy genre

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,7 @@
+-- Listing all Comdey show
+
+SELECT s.title FROM tv_genres g
+	JOIN tv_show_genres sg ON g.id = sg.genre_id
+	JOIN tv_shows s ON s.id = sg.show_id
+	WHERE g.name = 'Comedy'
+	ORDER BY s.title
