73c989afdd165d5db3b868f25995c4e6b772604f
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-10-19 15:11:15 +0100

    display genre

---
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- to display all genere and its link
+
+SELECT g.name AS genre,
+COUNT(show_id) AS number_of_row
+FROM tv_genres g
+JOIN tv_show_genres sg
+ON g.id = sg.genre_id
+GROUP BY genre
+ORDER BY number_of_row DESC;
