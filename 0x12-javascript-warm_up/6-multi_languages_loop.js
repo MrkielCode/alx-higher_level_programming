c5b4bd1b877ff06e27e2ae2443b916ca473ed918
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-13 20:46:26 +0100

    multi

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * To print array of integer
+ */
+
+const string = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i;
+
+for (i = 0; i < string.length; i++) {
+  console.log(string[i]);
+}
