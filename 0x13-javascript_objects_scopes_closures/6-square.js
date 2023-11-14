98a5c90bfe86afc7206ea7dd8b11d6c42b30a1b5
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-14 22:19:27 +0100

    square 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.width; i++) {
+      if (c === undefined) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log('C'.repeat(this.height));
+      }
+    }
+  }
+}
+module.exports = Square;
