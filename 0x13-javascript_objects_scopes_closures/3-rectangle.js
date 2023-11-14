26e603a31c5698db967e39c78399576eff2ead97
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-14 17:52:22 +0100

    3 rectangle

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      for (let k = 0; k < this.width; k++) {
+        process.stdout.write('x');
+      }
+      console.log();
+    }
+  }
+}
+module.exports = Rectangle;
