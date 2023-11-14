c39fc29cd58ac54b76e9c5bc1ec2c6e3513a75fd
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-14 22:43:27 +0100

    6 sqaure

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -4,9 +4,10 @@ module.exports = class Square extends require('./5-square') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
-    }
-    for (let i = 0; i < this.height; i++) {
-      console.log('C'.repeat(this.width));
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
     }
   }
 };
