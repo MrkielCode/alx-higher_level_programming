5a8337bdd52571f3c294997193b999918daf1a28
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-14 22:32:13 +0100

    square 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
-const Squares = require('./5-square');
 
-class Square extends Squares {
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    }
     for (let i = 0; i < this.height; i++) {
-      if (c === undefined) {
-        console.log('X'.repeat(this.width));
-      } else {
-        console.log('C'.repeat(this.width));
-      }
+      console.log('C'.repeat(this.width));
     }
   }
-}
-module.exports = Square;
+};
