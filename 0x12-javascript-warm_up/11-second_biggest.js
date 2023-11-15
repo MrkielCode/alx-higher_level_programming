6e4ccbca5d17f4c3a20b5dd229b21b78f9ef8693
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-15 02:47:49 +0100

    second biggest

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2).map(Number);
+
+let firstMax = -Infinity;
+let secondMax = -Infinity;
+
+if (args.length < 2) {
+  console.log(0);
+} else if (args.length === 2) {
+  console.log(0);
+} else {
+  for (let i = 0; i < args.length; i++) {
+    if (args[i] > firstMax) {
+      secondMax = firstMax;
+      firstMax = args[i];
+    } else if (args[i] < firstMax && args[i] > secondMax) {
+      secondMax = args[i];
+    }
+  }
+  console.log(secondMax);
+}
