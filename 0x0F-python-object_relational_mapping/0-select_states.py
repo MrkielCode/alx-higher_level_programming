6948eca1c669695e3e8510683a6ae0f3f2271dc7
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-19 22:39:15 +0100

    select state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,16 +7,13 @@ from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    my_user = argv[1]
-    my_password = argv[2]
-    my_db = argv[3]
 
     conn = MySQLdb.connect(
         host='localhost',
         port=3306,
-        user=my_user,
-        passwd=my_password,
-        db=my_db
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
     )
 
     cur = conn.cursor()
