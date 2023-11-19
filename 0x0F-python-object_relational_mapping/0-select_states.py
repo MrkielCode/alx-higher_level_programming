5ddd7e2d0aaca96167e1d83f0556fc77d49a6dd9
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-19 22:43:42 +0100

    select state

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -9,7 +9,7 @@ import MySQLdb
 if __name__ == "__main__":
 
     conn = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
         user=argv[1],
         passwd=argv[2],
