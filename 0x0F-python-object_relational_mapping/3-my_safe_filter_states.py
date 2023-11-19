95b01cd54a98ccd26a4248b46eae0b43c27937ad
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-20 00:02:33 +0100

    safe filter

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+script to select state that start with capital N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+    statement = "SELECT * FROM states WHERE name LIKE BINARY \
+    '{}' ORDER BY Id ASC".format(argv[4])
+    cur.execute(statement)
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        print(row)
+    cur.close()
+    conn.close()
