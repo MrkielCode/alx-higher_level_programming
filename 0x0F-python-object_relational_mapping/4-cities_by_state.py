787643344b99166b3f2eb5f35f44f573aeef69bb
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-20 00:51:10 +0100

    cities

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     )
     cur = conn.cursor()
     statement = '''
-    SELECT cities.id, cities.name, states.name 
-    FROM cities
-    JOIN states ON cities.state_id = states.id 
+    SELECT cities.id, cities.name, states.name
+    FROM cities \
+    JOIN states ON cities.state_id = states.id \
     ORDER BY cities.id ASC
     '''
 
