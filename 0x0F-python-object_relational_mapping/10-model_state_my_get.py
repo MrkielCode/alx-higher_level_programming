7cbbbf98867b8673851e1ff42f8786925c1c4551
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-20 14:56:16 +0100

    print by id

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This class script a state that contain "a"
+in database using ORM
+
+"""
+
+from sys import argv
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    """
+    Access the database and listing the state
+    """
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    state_name = argv[4]
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    query = session.query(State).filter(State.name.like(
+        state_name)).order_by(State.id).first()
+    if query is None:
+        print("Not found")
+    else:
+        print("{}".format(query.id))
+    session.close()
