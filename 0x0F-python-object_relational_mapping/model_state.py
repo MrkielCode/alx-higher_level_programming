bcd7e25910f7c441df5c70d46240b8566fb87fe3
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-20 13:14:25 +0100

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -13,10 +13,14 @@ Base = declarative_base(mymetadata)
 
 
 class State(Base):
+    """State class
+
+    Attributes:
+        __tablename__ (str): The table name of the class
+        id (int): The State id of the class
+        name (str): The State name of the class
     """
-    class representaion of state class
-    with instance of the class
-    """
+
     __tablename__ = 'states'
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
