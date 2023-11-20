f869c796e122c4b10bfe9d1583dfd77e09b9af53
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-11-20 13:00:29 +0100

    model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,20 +3,18 @@
 To create a state table and its instance
 
 """
-import sqlalchemy
-from sqlalchemy import create_engine, MetaData
-from sqlalchemy import Table, Column, Integer, String
+
+from sqlalchemy import Column, Integer, String, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 
 mymetadata = MetaData()
 Base = declarative_base(mymetadata)
 
-
 class State(Base):
     """
-    instance of the table state
-    """
+    class of each with instance of the table state
 
+    """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
