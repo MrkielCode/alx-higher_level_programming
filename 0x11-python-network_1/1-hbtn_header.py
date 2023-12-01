e870a52ad01cfb4fb8b9d3ca6e529a93e6e9b324
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-01 17:36:59 +0100

    header

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,8 +4,8 @@ It send request to server and it returns the X-Request-Id
 """
 from urllib.request import urlopen
 import sys
-
-url = sys.argv[1]
-with urlopen(url) as response:
-    x_request_id = response.headers.get('X-Request-Id')
-    print(x_request_id)
+if __name__ == "__main__":
+    url = sys.argv[1]
+    with urlopen(url) as response:
+        x_request_id = response.headers.get('X-Request-Id')
+        print(x_request_id)
