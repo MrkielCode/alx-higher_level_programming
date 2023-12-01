1ba1dcfc9db7a74e6147eace568b5ffc4f635107
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-01 16:34:01 +0100

    status

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,7 +3,7 @@
 sending a request and getting a status code
 
 """
-from urllib.request import Request, urlopen
+from urllib.request import urlopen
 
 with urlopen('https://alx-intranet.hbtn.io/status') as response:
     status = response.getcode()
