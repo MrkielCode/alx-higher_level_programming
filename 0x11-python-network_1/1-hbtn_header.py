c28fa5c5e2a94308dca20f584beda8360332710b
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-01 17:15:42 +0100

    header

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" It send request to get the request ID """
+from urllib.request import urlopen
+from sys import argv
+
+with urlopen(argv[1]) as response:
+    x_request_id = response.headers.get('X-Request-Id')
+    print(x_request_id)
