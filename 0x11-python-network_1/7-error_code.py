e554410f86c9da1d48045347d015f44e48169f49
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-02 00:39:36 +0100

    errorcode

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This script send a request that return status code
+then check if the status >= 400
+
+"""
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    response = requests.get(argv[1])
+    if (response.status_code >= 400):
+        print("Error code: {}".format(response.status_code))
+    else:
+        print(response.text)
