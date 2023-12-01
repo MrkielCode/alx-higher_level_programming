f355070f7fca55795e8679db399df9620f72a26b
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-01 23:12:21 +0100

    error3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script sends a request and return error
+if not found
+"""
+import urllib.request
+import urllib.error
+from sys import argv
+
+if __name__ == "__main__":
+    url = argv[1]
+    try:
+        data = urllib.request.Request(url)
+        with urllib.request.urlopen(data) as response:
+            body = response.read().decode('utf-8')
+            print(body)
+    except urllib.error.HTTPError as e:
+        print(e.code)
+        print(e.read())
