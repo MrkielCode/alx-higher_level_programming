7b4d8020e440cece454b2693a7e91d13d5f29779
Mrkielcode <ikechukwu.ezekiel@gmail.com>
2023-12-01 13:55:42 -0800

    email

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-
+"""
+This script sends request using POST method
+and retrives the email
+"""
 import urllib.request
 import urllib.parse
 from sys import argv
@@ -7,7 +10,7 @@ from sys import argv
 if __name__ == "__main__":
     link = argv[1]
     email = argv[2]
-    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    data = urllib.parse.urlencode({'email': email}).encode('ascii')
     req = urllib.request.Request(link, data=data, method='POST')
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
