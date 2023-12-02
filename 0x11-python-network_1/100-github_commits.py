41d477cfce189ff8ff307da8ad15f9cb97f7b144
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-02 19:05:11 +0100

    commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,12 +11,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         argv[2], argv[1])
 
-    r = requests.get(url)
-    commits = r.json()
-    try:
-        for i in range(10):
-            print("{}: {}".format(
-                commits[i].get("sha"),
-                commits[i].get("commit").get("author").get("name")))
-    except IndexError:
-        pass
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
