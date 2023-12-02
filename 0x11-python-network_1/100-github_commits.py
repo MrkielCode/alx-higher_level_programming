25f8468534e62fb14c60d7e7535a374bd9ceddb3
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-02 18:37:17 +0100

    fetch commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+To get a request of users in commit "rails"
+"""
+
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    repo_name = argv[1]
+    owner = argv[2]
+    url = "https://api.github.com/repos/{}/{}/commits".format(repo_name, owner)
+    headers = {
+        'Accept': 'Accept: application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        commits = response.json()
+        for commit in commits[:10]:
+            sha = commit['sha']
+            author_name = commit['commit']['author']['name']
+            print(f"{sha}: {author_name}")
