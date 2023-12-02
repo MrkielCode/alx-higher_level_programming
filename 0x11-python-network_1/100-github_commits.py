b49d74ac63f2a36db332694f518777940b95d6bc
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-02 18:55:21 +0100

    commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -10,12 +10,17 @@ if __name__ == "__main__":
     repo_name = argv[1]
     owner = argv[2]
     url = "https://api.github.com/repos/{}/{}/commits".format(repo_name, owner)
+    headers = {
+        'Accept': 'Accept: application/vnd.github+json',
+        'X-GitHub-Api-Version': '2022-11-28'
+    }
+
     response = requests.get(url)
     if response.status_code == 200:
         commits = response.json()
-        for commit in commits[:10]:
-            sha = commit['sha']
-            author_name = commit['commit']['author']['name']
-            print("{}: {}".format(sha, author_name))
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
     else:
         pass
