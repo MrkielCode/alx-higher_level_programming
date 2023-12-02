896b4d052dc5bf3217143733c301e5fcf0747062
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-02 18:59:50 +0100

    commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,22 +1,17 @@
 #!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository.
+Usage: ./100-github_commits.py <repository name> <repository owner>
 """
-To get a request of users in commit "rails"
-"""
-
+import sys
 import requests
-from sys import argv
+
 
 if __name__ == "__main__":
-    repo_name = argv[1]
-    owner = argv[2]
-    url = "https://api.github.com/repos/{}/{}/commits".format(repo_name, owner)
-    headers = {
-        'Accept': 'Accept: application/vnd.github+json',
-        'X-GitHub-Api-Version': '2022-11-28'
-    }
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
 
-    response = requests.get(url)
-    commits = response.json()
+    r = requests.get(url)
+    commits = r.json()
     try:
         for i in range(10):
             print("{}: {}".format(
