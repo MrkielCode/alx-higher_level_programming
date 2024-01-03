c6fb78947ff107b3847a0d1474f44efbca856191
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-01-03 19:10:11 +0100

    status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -9,12 +9,8 @@ const url = process.argv[2];
 
 request(url, function (error, response) {
   if (error) {
-    console.error(error);
+    console.error('Error:', error);
     return;
   }
-  if (response.statusCode === 200) {
-    console.log(`code: ${response.statusCode}`);
-  } else {
-    console.log(`doesnt_exist\ncode: ${response.statusCode}`);
-  }
+  console.log(`code: ${response.statusCode}`);
 });
