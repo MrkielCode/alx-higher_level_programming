36033531a97aa1311b53eba96889b06cedada31a
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-01-03 22:30:28 +0100

    request and write

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,34 +3,33 @@
  * script to get contents of web and save in a file
  */
 
-const request = require("request");
+const request = require('request');
 const fs = require('fs');
 
 const url = process.argv[2];
 const filePath = process.argv[3];
 
-function getData(url, callback) {
-    request(url, function (error, response, body) {
-        if (error) {
-            callback(error);
-            return;
-        }
-        if (response.statusCode === 200) {
-            callback(null, body);
-        }
-    });
+function getData (url, callback) {
+  request(url, function (error, response, body) {
+    if (error) {
+      callback(error);
+      return;
+    }
+    if (response.statusCode === 200) {
+      callback(null, body);
+    }
+  });
 }
 
-getData(url, function(error, body) {
-    if (error) {
-        console.error(error);
-        return;
-    }
+getData(url, function (error, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
 
-    fs.writeFile(filePath, body, function(err) {
-        if (err) {
-            console.error(err);
-            return;
-        }
-    });
+  fs.writeFile(filePath, body, function (err) {
+    if (err) {
+      console.error(err);
+    }
+  });
 });
