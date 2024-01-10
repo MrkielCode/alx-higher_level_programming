ad4ae4708a6498c98137027d3444b2f5f557e9f0
MrkielCode <ikechukwu.ezekiel@gmail.com>
2024-01-10 17:45:28 +0100

    append list

---
+++ 0x15-javascript-web_jquery/5-script.js
@@ -0,0 +1,10 @@
+/* global $ */
+/**
+ * add element to list
+ */
+
+$(document).ready(function () {
+  $('DIV#add_item').on('click', function () {
+    $('UL.my_list').append('<li>Item</li>');
+  });
+});
